9a501f4f9f12cd5b28a11033e1a6009514859e61
lukas0brunner <143714479+lukas0brunner@users.noreply.github.com>
2024-06-14 14:28:16 +0200

    add Entinity relations of User, Role and Authority

--- rest-food/rest-food/src/main/java/ch/noseryoung/rest_food/domain/role/Role.java
+++ rest-food/rest-food/src/main/java/ch/noseryoung/rest_food/domain/role/Role.java
@@ -1,4 +1,31 @@
 package ch.noseryoung.rest_food.domain.role;
 
+import ch.noseryoung.rest_food.domain.authority.Authority;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "role_authorities",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id")
+    )
+    private Set<Authority> authorities;
+
 }
