e2eff5a7c493e7f47c3301069c1f4a3bbd12158e
TahssinAl <143715323+Tahssinal-Noser@users.noreply.github.com>
2024-06-20 09:40:19 +0200

    Create MenucardItem entity

--- rest-food/rest-food/src/main/java/ch/noseryoung/rest_food/domain/menucard/menucarditem/MenucardItem.java
+++ rest-food/rest-food/src/main/java/ch/noseryoung/rest_food/domain/menucard/menucarditem/MenucardItem.java
@@ -1,5 +1,7 @@
-package ch.noseryoung.rest_food.domain.menucard;
+package ch.noseryoung.rest_food.domain.menucard.menucarditem;
 
+import ch.noseryoung.rest_food.domain.menucard.Menucard;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,11 +9,11 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(name ="MenucardItem")
 public class MenucardItem {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long menucarditemId;
 
     @Column(name = "name", nullable = false)
     private String name;
@@ -25,10 +27,8 @@ public class MenucardItem {
     @Column(name = "category", nullable = false)
     private String category;
 
-    @Column(name = "chefsChoice", nullable = false)
+    @Column(name = "chefs_choice", nullable = false)
     private Boolean chefsChoice;
 
-    @ManyToOne
-    @JoinColumn(name = "menucard_id", nullable = false)
-    private Menucard menucard;
+
 }
