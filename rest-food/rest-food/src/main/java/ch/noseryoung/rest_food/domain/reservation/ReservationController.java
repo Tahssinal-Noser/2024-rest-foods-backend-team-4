285d037f9b893c220e60715061ca50dfdae33bf8
TahssinAl <143715323+Tahssinal-Noser@users.noreply.github.com>
2024-06-25 15:59:45 +0200

    Added detailed Swagger documentation (@Operation and @ApiResponses) to all endpoints

--- rest-food/rest-food/src/main/java/ch/noseryoung/rest_food/domain/reservation/ReservationController.java
+++ rest-food/rest-food/src/main/java/ch/noseryoung/rest_food/domain/reservation/ReservationController.java
@@ -1,6 +1,8 @@
 package ch.noseryoung.rest_food.domain.reservation;
 
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,37 +13,76 @@ import java.util.List;
 @RequestMapping("/reservations")
 public class ReservationController {
 
-private final ReservationService reservationService;
+    private final ReservationService reservationService;
+
     @Autowired
     public ReservationController(ReservationService reservationService) {
         this.reservationService = reservationService;
     }
 
-
     @PostMapping
+    @Operation(
+            summary = "Create a new reservation",
+            description = "This endpoint allows you to create a new reservation by providing the reservation details."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Reservation created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data")
+    })
     public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation) {
         return ResponseEntity.status(201).body(reservationService.saveReservations(reservation));
     }
 
     @PutMapping("/{reservationId}")
+    @Operation(
+            summary = "Update an existing reservation by ID",
+            description = "This endpoint allows you to update the details of an existing reservation by providing the reservation ID and the updated details."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Reservation updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Reservation not found"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data")
+    })
     public ResponseEntity<Reservation> updateReservation(@PathVariable Long reservationId, @RequestBody Reservation reservationDetails) {
-        return ResponseEntity.status(201).body(reservationService.updateReservation(reservationId, reservationDetails));
+        return ResponseEntity.status(200).body(reservationService.updateReservation(reservationId, reservationDetails));
     }
 
     @GetMapping("/{reservationId}")
+    @Operation(
+            summary = "Get a reservation by ID",
+            description = "This endpoint allows you to retrieve the details of a reservation by providing the reservation ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Reservation retrieved successfully"),
+            @ApiResponse(responseCode = "404", description = "Reservation not found")
+    })
     public ResponseEntity<Reservation> getReservationById(@PathVariable Long reservationId) {
         return ResponseEntity.status(200).body(reservationService.getReservationById(reservationId));
     }
 
     @GetMapping
+    @Operation(
+            summary = "Get all reservations",
+            description = "This endpoint allows you to retrieve a list of all reservations."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Reservations retrieved successfully")
+    })
     public ResponseEntity<List<Reservation>> getAllReservations() {
         return ResponseEntity.status(200).body(reservationService.getAllReservations());
     }
 
     @DeleteMapping("/{reservationId}")
+    @Operation(
+            summary = "Delete a reservation by ID",
+            description = "This endpoint allows you to delete a reservation by providing the reservation ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Reservation deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Reservation not found")
+    })
     public ResponseEntity<Void> deleteReservation(@PathVariable Long reservationId) {
         reservationService.deleteReservation(reservationId);
         return ResponseEntity.status(204).build();
     }
 }
-
--- rest-food/rest-food/src/main/java/ch/noseryoung/rest_food/domain/reservation/table/RestaurantTableController.java
+++ rest-food/rest-food/src/main/java/ch/noseryoung/rest_food/domain/reservation/table/RestaurantTableController.java
@@ -1,7 +1,8 @@
 package ch.noseryoung.rest_food.domain.reservation.table;
 
-
-import ch.noseryoung.rest_food.domain.reservation.Reservation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,37 +13,76 @@ import java.util.List;
 @RequestMapping("/tables")
 public class RestaurantTableController {
 
-        private final RestaurantTableService restaurantTableService;
+    private final RestaurantTableService restaurantTableService;
 
-        @Autowired
-        public RestaurantTableController(RestaurantTableService restaurantTableService) {
-            this.restaurantTableService = restaurantTableService;
-        }
+    @Autowired
+    public RestaurantTableController(RestaurantTableService restaurantTableService) {
+        this.restaurantTableService = restaurantTableService;
+    }
 
-        @PostMapping
-        public ResponseEntity<RestaurantTable> createTable(@RequestBody RestaurantTable restaurantTable) {
-            return ResponseEntity.status(201).body(restaurantTableService.saveTable(restaurantTable));
-        }
+    @PostMapping
+    @Operation(
+            summary = "Create a new restaurant table",
+            description = "This endpoint allows you to create a new restaurant table by providing the table details."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Table created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data")
+    })
+    public ResponseEntity<RestaurantTable> createTable(@RequestBody RestaurantTable restaurantTable) {
+        return ResponseEntity.status(201).body(restaurantTableService.saveTable(restaurantTable));
+    }
 
     @PutMapping("/{tableId}")
+    @Operation(
+            summary = "Update an existing restaurant table by ID",
+            description = "This endpoint allows you to update the details of an existing restaurant table by providing the table ID and the updated details."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Table updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Table not found"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data")
+    })
     public ResponseEntity<RestaurantTable> updateTable(@PathVariable Integer tableId, @RequestBody RestaurantTable tableDetails) {
-        return ResponseEntity.status(201).body(restaurantTableService.updateTable(tableId, tableDetails));
+        return ResponseEntity.status(200).body(restaurantTableService.updateTable(tableId, tableDetails));
     }
 
     @GetMapping("/{tableId}")
+    @Operation(
+            summary = "Get a restaurant table by ID",
+            description = "This endpoint allows you to retrieve the details of a restaurant table by providing the table ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Table retrieved successfully"),
+            @ApiResponse(responseCode = "404", description = "Table not found")
+    })
     public ResponseEntity<RestaurantTable> getTableById(@PathVariable Integer tableId) {
         return ResponseEntity.status(200).body(restaurantTableService.getTableById(tableId));
     }
 
     @GetMapping
+    @Operation(
+            summary = "Get all restaurant tables",
+            description = "This endpoint allows you to retrieve a list of all restaurant tables."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tables retrieved successfully")
+    })
     public ResponseEntity<List<RestaurantTable>> getAllTables() {
         return ResponseEntity.status(200).body(restaurantTableService.getAllTables());
     }
 
     @DeleteMapping("/{tableId}")
+    @Operation(
+            summary = "Delete a restaurant table by ID",
+            description = "This endpoint allows you to delete a restaurant table by providing the table ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Table deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Table not found")
+    })
     public ResponseEntity<Void> deleteTable(@PathVariable Integer tableId) {
-       restaurantTableService.deleteTable(tableId);
+        restaurantTableService.deleteTable(tableId);
         return ResponseEntity.status(204).build();
     }
-
 }
